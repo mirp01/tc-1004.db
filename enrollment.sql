56b65bcd42a4bd73b8d398832e53068b9a1ec320
Mireya  Rodríguez <a01637218@tec.mx>
2023-10-18 16:17:12 +0000

    constraints

--- enrollment.sql
+++ enrollment.sql
@@ -22,7 +22,7 @@ create table profesor(
 -- Tabla curso
 create table curso(
     clave text primary key,
-    nombre text,
+    nombre text
 );
 
 -- Tabla grupo
@@ -30,13 +30,31 @@ create table grupo(
     id integer primary key,
     numero integer,
     cupo integer, 
-    curso_clave text foreign key,
-    profesor_nomina text foreign key
+    curso_clave text,
+    profesor_nomina text 
 );
 
 -- Tabla alumno_grupo
 create table alumno_grupo(
     id integer primary key, 
-    alumno_matricula text foreign key,
-    grupo_id integer foreign key, 
+    alumno_matricula text,
+    grupo_id integer
 );
+
+/*markdown
+Para agregar una llave foránea:
+*/
+
+/*
+ALTER TABLE orders
+    ADD CONSTRAINT fk_orders_customers FOREIGN KEY (customer_id) REFERENCES customers (id)
+    
+*/
+
+ALTER TABLE alumno_grupo
+    ADD CONSTRAINT fk_alumno_grupo_alumno FOREIGN KEY (alumno_matricula) REFERENCES alumno(matricula),
+    ADD CONSTRAINT fk_grupo_id_grupo FOREIGN KEY (grupo_id) REFERENCES grupo(id);
+
+ALTER TABLE grupo
+    ADD CONSTRAINT fk_curso_clave_curso FOREIGN KEY (curso_clave) REFERENCES curso(clave),
+    ADD CONSTRAINT fk_profesor_nomina_profesor FOREIGN KEY (profesor_nomina) REFERENCES profesor(nomina);
